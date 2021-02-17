5efbba7fbe23103eb1146bc73c7bffd74426608f
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-02-17 09:38:35 +0200

    Created application-localmysql.properties with configuration for service user. Modified the Beer entity's id constraints to avoid hibernate error. Excluded the ArtemisAutoConfiguration.class in the BeerOrderServiceApplication.

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-16T14:31:12+0200",
+    date = "2021-02-17T09:21:52+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.5 (Oracle Corporation)"
 )
 @Component
