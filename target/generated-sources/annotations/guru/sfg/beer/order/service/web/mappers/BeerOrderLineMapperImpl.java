a55e10703eab459569e2e3a0d8d8ba55e1be5d95
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-02-21 11:23:18 +0200

    Updated the parent pom.

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-17T09:21:52+0200",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.5 (Oracle Corporation)"
+    date = "2021-02-21T11:21:52+0200",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.5 (Oracle Corporation)"
 )
 @Component
 @Primary
