90ae69ae2299a7bfa39bc8e26c3e078b1cc09979
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-02-21 16:29:54 +0200

    Removed the exclusion for the Artemis autoconfiguration to solve the error where the context could not auto configure the JmsTemplate.

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-21T14:04:37+0200",
+    date = "2021-02-21T16:23:44+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.5 (Oracle Corporation)"
 )
 @Component
