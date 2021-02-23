efa12c75807601dd82ac6c05ee7c73428ad7e003
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-02-23 12:36:22 +0200

    Test failed validaiton.

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-23T10:12:22+0200",
+    date = "2021-02-23T12:32:18+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.5 (Oracle Corporation)"
 )
 @Component
